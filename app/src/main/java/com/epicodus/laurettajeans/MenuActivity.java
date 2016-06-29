3a41b9e6d42f1f5d994e686439a7f475c14cf16a
Noah Kittleson and Nadiya Yeroshkina <nadiya@>
2016-06-29 15:34:08 -0700

    first mainActivity passes

--- app/src/main/java/com/epicodus/laurettajeans/MenuActivity.java
+++ app/src/main/java/com/epicodus/laurettajeans/MenuActivity.java
@@ -2,12 +2,28 @@ package com.epicodus.laurettajeans;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MenuActivity extends AppCompatActivity {
+
+    @Bind(R.id.listView) ListView mListView;
+    private String[] pies = new String[] {"Strawberry Sweet Cream", "Blackberry Raspberry Streusel",
+            "Tart Cherry", "Rhubarb Streusel", "Rhubarb Raspberry Sour Cream", "Blueberry Rhubarb",
+            "Strawberry Rhubarb", "Chocolate Cream", "Coconut Cream", "Chocolate Banana Cream",
+            "Banana Cream", "Blackbottom Coconut", "Key Lime", "Lemon Gingersnap", "Salted Honey",
+            "Honey Hazelnut", "Chocolate Chess", "S'mores", "Strawberry Short Pie", "Lemon Meringue"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, pies);
+        mListView.setAdapter(adapter);
     }
 }
---
+++ app/src/test/java/com/epicodus/laurettajeans/MainActivityTest.java
@@ -0,0 +1,34 @@
+package com.epicodus.laurettajeans;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Guest on 6/29/16.
+ */
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView mainTextView = (TextView) activity.findViewById(R.id.AppNameTextView);
+        assertTrue("LaurettaJean's".equals(mainTextView.getText().toString()));
+    }
+}
