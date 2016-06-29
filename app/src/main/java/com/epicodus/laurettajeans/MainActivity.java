bb664893c3c7df4dd42c6f0291fbe211bce59790
Noah Kittleson and Nadiya Yeroshkina <noah@>
2016-06-29 13:58:29 -0700

    add manueActivity, used butterKnife

--- app/src/main/java/com/epicodus/laurettajeans/MainActivity.java
+++ app/src/main/java/com/epicodus/laurettajeans/MainActivity.java
@@ -1,20 +1,33 @@
 package com.epicodus.laurettajeans;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-   private TextView mAppNameTextView;
+    @Bind(R.id.AppNameTextView) TextView mAppNameTextView;
+    @Bind(R.id.menueButton) Button mPieMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAppNameTextView = (TextView) findViewById(R.id.AppNameTextView);
+        ButterKnife.bind(this);
         Typeface pacifico = Typeface.createFromAsset(getAssets(),"fonts/Pacifico.ttf");
         mAppNameTextView.setTypeface(pacifico);
+        mPieMenuButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+        startActivity(intent);
     }
 }
